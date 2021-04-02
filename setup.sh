b9df0cdb8c457366e2f5b7058c3922fc34678f4f
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-02 17:02:57 -0300

    feat: adds definitions of rabbitmq to docker-compose

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d tmp ]; then
+  sudo rm -rf tmp
+  echo "Directory /tmp was deleted"
+fi
+
+docker-compose build
+
+docker-compose run consumer bundle exec rake db:create 
+docker-compose run producer bundle exec rake db:setup
