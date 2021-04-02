a65a26bf60c9a1597c29a689540bc109d2c76cb2
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-02 18:40:20 -0300

    feat: adds bookstore-api

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ docker-compose build
 docker-compose run usms bundle exec rake db:create
 docker-compose run ntms bundle exec rake db:create
 docker-compose run oms bundle exec rake db:create
+docker-compose run api bundle exec rake db:create
