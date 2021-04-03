9a7fb19d92d0f5f636671b3c037d191b76bebcd3
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-03 10:45:44 -0300

    feat: seup to run webpacker

--- setup.sh
+++ setup.sh
@@ -4,3 +4,17 @@ docker-compose run ntms bundle exec rake db:create
 docker-compose run usms bundle exec rake db:setup
 docker-compose run oms bundle exec rake db:setup
 docker-compose run api bundle exec rake db:setup
+
+
+if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+  docker-compose run ntms bundle exec rails webpacker:install
+fi
+if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+  docker-compose run usms bundle exec rails webpacker:install
+fi
+if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+  docker-compose run oms bundle exec rails webpacker:install
+fi
+if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+  docker-compose run api bundle exec rails webpacker:install
+fi
