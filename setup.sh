6b7e2fb9a69713a4c605d7a9f807bd8af50b7674
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-04 09:22:48 -0300

    feat: config rake tasks

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-docker-compose run ntms bundle exec rake db:create
-docker-compose run usms bundle exec rake db:setup
-docker-compose run oms bundle exec rake db:setup
-docker-compose run api bundle exec rake db:setup
+# docker-compose run ntms bundle exec rake db:create
+# docker-compose run usms bundle exec rake db:setup
+# docker-compose run oms bundle exec rake db:setup
+# docker-compose run api bundle exec rake db:setup
 
 
 if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
   docker-compose run ntms bundle exec rails webpacker:install
 fi
-if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+if [ ! -f ./users-ms/config/webpacker.yml ]; then
   docker-compose run usms bundle exec rails webpacker:install
 fi
-if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+if [ ! -f ./orders-ms/config/webpacker.yml ]; then
   docker-compose run oms bundle exec rails webpacker:install
 fi
-if [ ! -f ./notifications-ms/config/webpacker.yml ]; then
+if [ ! -f ./bookstore-api/config/webpacker.yml ]; then
   docker-compose run api bundle exec rails webpacker:install
 fi
--- users-ms/lib/tasks/users.rake
+++ users-ms/lib/tasks/users.rake
@@ -10,6 +10,37 @@ namespace :user do
     end
   end
 
+  desc 'update an user'
+  task :update, [:user_id, :name, :surname] => :environment do |task, args|
+    begin
+      user_id = args[:user_id].to_i
+      hashed_attributes = {
+        surname: args[:surname],
+        name: args[:name]
+      }
+
+      User::UpdateInteractor.new.execute!(
+        hashed_attributes: hashed_attributes,
+        user_id: user_id
+      )
+    rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid => error
+      puts "Exception rescue: #{error.message}"
+    end
+  end
+
+  desc 'list all users'
+  task :list, [] => :environment do |task, args|
+    begin
+      users = User.all.map do |user|
+        user.attributes
+      end
+
+      puts "USERS: #{users}"
+    rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid => error
+      puts "Exception rescue: #{error.message}"
+    end
+  end
+
   desc 'Count users'
   task :count, [] => :environment do |task, args|
     puts "Quantity of Users: #{User.count}"
