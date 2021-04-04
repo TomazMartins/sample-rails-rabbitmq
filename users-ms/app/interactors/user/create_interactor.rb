352e86ffad7ff30447fe0f211f33164ac3273e0f
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-04 09:28:23 -0300

    feat: adds publish inside user_create_interactor

--- users-ms/app/interactors/user/create_interactor.rb
+++ users-ms/app/interactors/user/create_interactor.rb
@@ -5,5 +5,8 @@ class User::CreateInteractor
     @user = FactoryBot.create(:user)
 
     return @user
+  ensure
+    serialized_user = Api::V1::UserSerializer.new(@user).as_json
+    Rabbitmq::Publisher::UserEvent.new(:created).publish(serialized_user)
   end
 end
