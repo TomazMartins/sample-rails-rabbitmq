ea14ae70a35cb0e49d30619578a08dc035ee9930
Tomaz Martins <tomaz@todocartoes.com.br>
2021-04-08 13:39:49 -0300

    feat: removes producer

--- producer/app/serializers/api/v1/order_serializer.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::OrderSerializer
-  include FastJsonapi::ObjectSerializer
-  set_type :order
-
-  attributes :identifier, :total, :status
-end
--- producer/db/migrate/20210323212655_create_order.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrder < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.string :identifier
-      t.decimal :total
-      t.string :status
-    end
-  end
-end
